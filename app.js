183c30e0cf0a223993a845741b7ce98d0c71c5eb
brian mcgrane <brianmcgrane@brians-MacBook-Pro.local>
2017-08-20 09:17:31 -0700

    jquery

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+  console.log("ready!");
 });
 
 
-$(".mainImg").hide(2000).show(2000);
+$(".mainImg").hide(2000).show(2000).fadeOut(1000).fadeIn(1000);
